7630f0de3400139f6eca6744c4bc1fac78c81943
Tjoma <temaswaga@mail.ru>
2026-01-01 23:01:52 +0300

    Create the project

---
+++ src/test/java/org/cloudstorage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
